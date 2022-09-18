d37aac6d6885a497a73665aef5c34668614ae918
wallace-junio <wallacejunio2014@hotmail.com>
2022-09-18 01:21:57 -0300

    Desafio concluido

---
+++ 2738.sql
@@ -0,0 +1,7 @@
+---Developed by: @owallacejr
+
+select c.name ,trunc((((math*2)+(specific*3)+(project_plan*5))/10), 2) as avg
+from candidate c 
+inner join score s 
+on c.id =s.candidate_id 
+order by avg desc
